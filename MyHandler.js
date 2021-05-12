cd385e7d4d283c85b97271a1107d60ee93cd7244
kidykjh <kidykjh@gmail.com>
2021-05-12 09:55:51 +0900

    add Random wait page

--- MyHandler.js
+++ MyHandler.js
@@ -1,32 +1,45 @@
 function start(res) {
-    let Body = '<head><meta charset="UTF-8"/></head>';
+    let body = '<head><meta charset="UTF-8"/></head>';
     body += '<body><div>Hello world!<br>I am in the cloud class<br></div>'
     body += '<div><a href="hello">hello 페이지</a></div>'
     body += '<div><a href="wait">5초 대기 페이지</a></div>'
+    body += '<div><a href="randomWait">무작위 대기 페이지</a></div>'
     body += '</body>'
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(sBody);
+    res.write(body);
     res.end();
 }
 
 function hello(res) {
-    let Body = 'This is my first web server.';
+    let body = 'This is my first web server.';
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(sBody);
+    res.write(body);
     res.end();
 }
 
 function wait(res) {
     setTimeout(function() {
-        let Body = 'Thank you for waiting for 5 seconds.';
+        let body = 'Thank you for waiting for 5 seconds.';
 
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(sBody);
+        res.write(body);
         res.end();
     }, 5000);
+}
+
+function randomWait(res) {
+    let waitTime = Math.round(Math.random() * 10000.);
+    setTimeout(function() {
+        let body = 'Thank you for waiting for ' + waitTime + ' seconds.';
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(body);
+        res.end();
+    }, waitTime);
 }
 
 exports.start = start;
 exports.hello = hello;
 exports.wait = wait;
+exports.randomWait = randomWait;
